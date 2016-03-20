8ae39c4b81f1c536f1aa1d346586d838df58485e
Aaron Harvey <harvnlenny@gmail.com>
2016-03-20 13:51:50 -0400

    Built a Resume Header with .replace and .append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Aaron Harvey");
+
+// [string].replace([old], [new])
+
+// var email = "cameron@udacity.com";
+
+// var newEmail = email.replace("udacity", "gmail");
+
+// console.log(email);
+// console.log(newEmail);
+
+// var awesomeThoughts = "My name is Cameron and I am AWESOME!";
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Aaron Harvey");
+
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").append(formattedRole);
